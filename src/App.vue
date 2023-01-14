b9d0ecc87079606d18594f0c71d36c32a3a93472
Leo Serrao Barbosa <leoserrao1981@gmail.com>
2023-01-14 15:43:11 -0300

    primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container grid-lg my-2 py-2 text-dark">
+    <h1>Consumindo API de Cotações de Moedas</h1>
+    <div class="card">
+      <div class="card-header">
+        <div class="h4">Exibindo as Moedas</div>
+      </div>
+      <div class="card-body">
+        <HelloWorld :quotes="quotes" />
+      </div>
+
+    </div> 
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import api from "@/servidor/api.js";
+import {onMounted, reactive, toRefs} from 'vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+
+  //Expor os dados da API no component
+  setup() {
+    const data = reactive ({ // dados reativos
+      quotes: { },
+    });
+
+    onMounted( async() => {
+      const response = await api.all();
+      data.quotes = response.data;
+    })
+
+    return { ...toRefs(data) }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .h4 {
+    text-align: center;
+    margin-bottom: 10px;
+  }
+
+  .card {
+    box-shadow: inset 0 0 1em gold, 0 0 1em red;
+  }
 </style>
---
+++ src/servidor/api.js
@@ -0,0 +1,17 @@
+// Configraões da API
+
+import axios from 'axios'
+
+const api = axios.create({
+
+    baseURL: 'https://economia.awesomeapi.com.br/json',
+    
+  });
+
+  export default {
+
+    all() {
+
+        return api.get('/all')
+    }
+  }
